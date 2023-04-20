bb625b8a41468bd106f33655d98eed81ff485c09
Lady Petra Takyi <misstakyi18@gmail.com>
2023-04-20 16:13:19 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Using betty for the first time in c
+ * Return: o (taskcompleted)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
