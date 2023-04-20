4ae6051440d8c28ef2273f430631ab3692372ded
Lady Petra Takyi <misstakyi18@gmail.com>
2023-04-20 16:17:39 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that prings with proper grammer
+ * Return: 0 (taskcompleted)
+ */
+int main(void)
+{
+     printf("with proper grammar, but the outcome is a piece of art,\n");
+     return (0); 
+ }
