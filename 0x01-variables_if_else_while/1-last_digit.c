00837337dbce57b14c901f5fb758d18555f1a780
Petra-Takyi <takyipetra01@gmail.com>
2023-04-21 14:46:56 -0700

    task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description - 'printing last digit number'
+ *Description - 'last digit'
+ *Return: always 0 (sucess)
+ */
+int main(void)
+@@ -28,3 +28,4 @@ int main(void)
+	}
+
+	return (0);
+}
