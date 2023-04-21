58bbf5a2ac7770319b2b52d4fa77a7d614e47452
Petra-Takyi <takyipetra01@gmail.com>
2023-04-21 15:00:08 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+void print alphabet (void)
+{
+	char c;
+	for (c = 'a' ; c <= z ;c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
