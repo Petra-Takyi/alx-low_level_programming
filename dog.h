4fbc21cccb66a8a29b4ae61bffada48fcb5723b3
Petra-Takyi <takyipetra01@gmail.com>
2023-05-22 12:14:04 -0700

    task 0

--- dog.h
+++ dog.h
@@ -2,24 +2,31 @@
 #define DOG_H
 
 /**
- * struct dog - Defines a new type dog
- * @name: member of type pointer to a character
- * @owner: member of type pointer to a character
- * @age: member of type float
- *
- * Description: Structure
- */
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*
+* Description: Longer description
+*/
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 };
+/**
+* dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
-void free_dog(dog_t *d);
+
+
 #endif
