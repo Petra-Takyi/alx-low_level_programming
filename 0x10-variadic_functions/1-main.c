b5316cfa3c756f926c80b7d849f069686f120785
Petra-Takyi <takyipetra01@gmail.com>
2023-05-25 08:45:58 -0700

    variadic

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
@@ -10,3 +11,4 @@ int main(void)
     print_numbers(", ", 4, 0, 98, -1024, 402);
     return (0);
 }
+
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,35 +1,31 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_numbers - Entry point
- * Description -'a function that prints numbers, followed by a new line'
- * @separator: The string to be printed between numbers.
- * @n: number of integers passed to the function
- * @...: A variable number of numbers to be printed
- *
- * Return: void
+ * Description: Prints numbers then a new line.
+ * @separator: string to be printed between numbers.
+ * @n: number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int h;
+	va_list nums;
+	unsigned int index;
 
-	va_list arguments;
+	va_start(nums, n);
 
-	va_start(arguments, n);
-
-	for (h = 0; h < n; h++)
+	for (index = 0; index < n; index++)
 	{
-		printf("%d", va_arg(arguments, int));
+		printf("%d", va_arg(nums, int));
 
-/* if separator is not null and h is not the last argument, print seperator*/
-
-		if (h != (n - 1) && separator != NULL)
+		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
-	va_end(arguments);
 
-	printf("\n"); /*Print a new line at the end of your function */
+	printf("\n");
+
+	va_end(nums);
 }
