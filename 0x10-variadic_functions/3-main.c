eade5d6981061fbcfc01cb02dbf08d3cb5046252
Petra-Takyi <takyipetra01@gmail.com>
2023-05-25 04:15:53 -0700

    0x10-variadic_functions

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+
+#include <stdio.h>
+
+int main(void)
+{
+    print_all("ceis", 'B', 3, "stSchool");
+    return 0;
+}
+
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
+
